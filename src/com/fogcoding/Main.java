b3614b645a0dbdff549d0acc992650b29cfef884
fogcoding <notesome@outlook.com>
2020-03-31 21:04:57 +0800

    init jdk source project

---
+++ src/com/fogcoding/Main.java
@@ -0,0 +1,17 @@
+package com.fogcoding;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("1","a");
+        System.out.println("running!");
+
+
+
+    }
+
+}
